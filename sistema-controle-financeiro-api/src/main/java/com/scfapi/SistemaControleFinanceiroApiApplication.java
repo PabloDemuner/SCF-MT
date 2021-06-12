ba32cac39300289b39302e418b2de83cbd04a73a
Pablo <pablodiegodemuner@gmail.com>
2021-06-12 12:16:57 -0400

    Projeto iniciado

---
+++ sistema-controle-financeiro-api/src/main/java/com/scfapi/SistemaControleFinanceiroApiApplication.java
@@ -0,0 +1,13 @@
+package com.scfapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaControleFinanceiroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaControleFinanceiroApiApplication.class, args);
+	}
+
+}
