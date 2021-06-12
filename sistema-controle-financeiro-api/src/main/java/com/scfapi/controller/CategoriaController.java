e23263a408e4dc0d320d6c1b5a56b78eb413b6ad
Pablo <pablodiegodemuner@gmail.com>
2021-06-12 15:33:04 -0400

    Tabela categoria/classes criadas

---
+++ sistema-controle-financeiro-api/src/main/java/com/scfapi/controller/CategoriaController.java
@@ -0,0 +1,50 @@
+package com.scfapi.controller;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.scfapi.model.Categoria;
+import com.scfapi.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaController {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@GetMapping
+	public List<Categoria> listar() {
+		return categoriaRepository.findAll();
+	}
+	
+	@PostMapping
+	//@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Categoria> adicionar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+				.buildAndExpand(categoriaSalva.getId()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Categoria> buscaId(@PathVariable Long id) {
+		Categoria categoria = categoriaRepository.findById(id).orElse(null);
+		return categoria != null ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build();	 
+	}
+}
---
+++ sistema-controle-financeiro-api/src/main/java/com/scfapi/repository/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package com.scfapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.scfapi.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+
+
+}
