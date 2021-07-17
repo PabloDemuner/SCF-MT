dadf59fd49aa9554c362174ef5cb847fe73be79a
Pablo <pablodiegodemuner@gmail.com>
2021-07-17 09:53:27 -0400

    FEAT: Criado tratamento de exceptions de lancamento

--- sistema-controle-financeiro-api/src/main/java/com/scfapi/exceptionhandler/ExceptionsHandler.java
+++ sistema-controle-financeiro-api/src/main/java/com/scfapi/exceptionhandler/ExceptionsHandler.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -102,5 +103,15 @@ public class ExceptionsHandler extends ResponseEntityExceptionHandler {
 			return mensagemDesenvolvedor;
 		}
 
+	}
+	
+	//Faz o tratamento de exceptions dos campos lançamentos
+	@ExceptionHandler({DataIntegrityViolationException.class})
+	public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex, WebRequest request) {
+		String mensagemUsuario = messageSource.getMessage("recurso.operacao-nao-permitida", null,
+				LocaleContextHolder.getLocale());
+		String mensagemDesenvolvedor = ex.getMostSpecificCause().toString();
+		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
 	}
 }
