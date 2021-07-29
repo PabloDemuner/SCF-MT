8222138e51c279aae9679ec3d5cc1672c4f7fb8a
Pablo <pablodiegodemuner@gmail.com>
2021-07-28 22:43:46 -0400

    FIX: Abilitado segurança nos metodos com o Oauth2

--- sistema-controle-financeiro-api/src/main/java/com/scfapi/config/ResourceServerConfig.java
+++ sistema-controle-financeiro-api/src/main/java/com/scfapi/config/ResourceServerConfig.java
@@ -1,14 +1,19 @@
 package com.scfapi.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecurityExpressionHandler;
 
 @Configuration
 @EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)//Habilita a segurança nos métodos.
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	
 	@Override
@@ -27,5 +32,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	public void configure(ResourceServerSecurityConfigurer resourcesConfig) throws Exception {
 		
 		resourcesConfig.stateless(true);
+	}
+	
+	//Efetua segurança dos metodos com o OAuth2
+	@Bean
+	public MethodSecurityExpressionHandler createExpressionHandler() {
+		return new OAuth2MethodSecurityExpressionHandler();
 	}
 }
