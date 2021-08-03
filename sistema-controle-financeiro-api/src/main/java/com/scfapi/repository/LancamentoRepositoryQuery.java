295f7ccfff171ddd924c93f2c963c4785bf14ef4
Pablo <pablodiegodemuner@gmail.com>
2021-08-03 19:26:26 -0400

    FEAT: Criado repository "resumir"  para resumo de lancamentos paginado.

--- sistema-controle-financeiro-api/src/main/java/com/scfapi/repository/LancamentoRepositoryQuery.java
+++ sistema-controle-financeiro-api/src/main/java/com/scfapi/repository/LancamentoRepositoryQuery.java
@@ -4,9 +4,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import com.scfapi.controller.filter.LancamentoFilter;
+import com.scfapi.controller.filter.ResumoLancamento;
 import com.scfapi.model.Lancamento;
 
 public interface LancamentoRepositoryQuery {
 
 	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
+	public Page<ResumoLancamento> resumir (LancamentoFilter lancamentoFilter, Pageable pageable);
 }
