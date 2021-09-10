680b432aac2a81e33e9e76782e5a9c2119f29195
Pablo <pablodiegodemuner@gmail.com>
2021-09-09 22:58:09 -0400

    FEAT: Adicionado anotação @EnableConfigurationProperties(ScfApiProperty.class)

--- sistema-controle-financeiro-api/src/main/java/com/scfapi/SistemaControleFinanceiroApiApplication.java
+++ sistema-controle-financeiro-api/src/main/java/com/scfapi/SistemaControleFinanceiroApiApplication.java
@@ -2,8 +2,12 @@ package com.scfapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.scfapi.config.property.ScfApiProperty;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ScfApiProperty.class)
 public class SistemaControleFinanceiroApiApplication {
 
 	public static void main(String[] args) {
