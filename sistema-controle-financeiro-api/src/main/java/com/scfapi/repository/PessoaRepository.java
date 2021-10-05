a6dc97d88e05376de6771bfcd2feef93de6f4f25
Pablo <pablodiegodemuner@gmail.com>
2021-10-05 16:32:29 -0400

    FEAT: Adicionado a anotacao @Repository

--- sistema-controle-financeiro-api/src/main/java/com/scfapi/repository/PessoaRepository.java
+++ sistema-controle-financeiro-api/src/main/java/com/scfapi/repository/PessoaRepository.java
@@ -1,9 +1,11 @@
 package com.scfapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.scfapi.model.Pessoa;
 
+@Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
 
 }
