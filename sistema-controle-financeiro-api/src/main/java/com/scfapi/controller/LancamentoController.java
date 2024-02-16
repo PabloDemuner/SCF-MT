49dc26d0720da8bff9e33f7cd3b39d5ebb8e10b2
Pablo Demuner <pablodiegodemuner@gmail.com>
2024-02-16 11:05:26 -0400

    Fix_Endpoint de resumo de lançamentos

--- sistema-controle-financeiro-api/src/main/java/com/scfapi/controller/LancamentoController.java
+++ sistema-controle-financeiro-api/src/main/java/com/scfapi/controller/LancamentoController.java
@@ -13,7 +13,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -25,7 +24,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -41,6 +39,7 @@ import com.scfapi.repository.LancamentoRepository;
 import com.scfapi.service.LancamentoService;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @Api(tags = "Lancamentos")
@@ -76,7 +75,8 @@ public class LancamentoController {
 		return lancamentoRepository.filtrar(lancamentoFilter, pageable);
 	}
 
-	@GetMapping(path = "resumo", produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(params = "resumo", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiOperation(value = "Resumo de lançamentos", hidden = true)
 	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO')")
 	public Page<ResumoLancamento> resumir(LancamentoFilter lancamentoFilter, Pageable pageable) {
 		return lancamentoRepository.resumir(lancamentoFilter, pageable);
